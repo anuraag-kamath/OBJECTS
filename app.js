ed2e08c8c23705db6266ad2a77f71357e3404d7e
Anuraag Kamath <anuraag.kamath@gmail.com>
2018-09-03 13:18:02 +0530

    Final Commit to Objects

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var app = express()
 
 const port = process.env.OBJ_PORT || 12004;
 const jwt_key = process.env.JWT_KEY || "alphabetagamma"
+const logging_enabled = process.env.LOGGING_ENABLED || false
 
 
 //newImports
@@ -24,22 +25,44 @@ app.use(bodyparser.urlencoded({
     extended: true
 }))
 
+// logger = (activity, subActivity, subsubActivity, activityId, status, userId, ipAddress, method) => {
+//     if (logging_enabled == true) {
+//         if (userId.length > 0) {
+//             user.findById(userId, (err, res1) => {
+//                 if (res1 != undefined && res1 !== 'undefined' && res1.user != undefined && res1.user !== 'undefined') {
+//                     act = new userActivity({
+//                         activity, subActivity, subsubActivity, activityId, status, userId, user: res1.user.username, ipAddress, method, logDate: new Date(), domain: "OBJECTS"
+//                     });
+//                     act.save();
+
+//                 }
+
+
+//             })
+
+//         } else {
+//             act = new userActivity({
+//                 activity, subActivity, subsubActivity, activityId, status, userId, user: "", ipAddress, method, logDate: new Date(), domain: "OBJECTS"
+//             });
+//             act.save();
+
+//         }
+
+//     }
+// }
 
 app.post('/objects', (req, res) => {
-    console.log("**/objects entered**");
-    console.log(req.body.schemaName);
-    console.log(req.body)
+    //logger("API", "objects", "", req.body.schemaName, "success", "", req.connection.remoteAddress, "POST");
+
     fs.appendFile('./schemas/' + req.body.schemaName + '.js', "var mongoose=require('mongoose');\nvar " + req.body.schemaName + '=mongoose.model("' + req.body.schemaName + '",{"' + req.body.schemaName + '":[' + JSON.stringify(req.body.schemaStructure) + '],"instanceId":{"type":"String"}});\nmodule.exports={' + req.body.schemaName + "}", (err) => {
-        console.log(err);
     })
 
     fs.readFile('./app.js', (err, data) => {
         data = String(data).replace("//newImports", "//newImports\nvar {" + req.body.schemaName + "}=require('./schemas/" + req.body.schemaName + "')");
-        data = String(data).replace("//newSettersGetter" + "s", "//newSettersGetter" + "" + "s\n\napp.post('/" + req.body.schemaName + "', (req, res) => {\n\tconsole.log(req.body);\n\tvar obj1 = new " + req.body.schemaName + "(req.body);\n\tconsole.log(obj1)\n\obj1.save().then((doc) => {\n\t\tres.send(`${doc}`);\n\t})\n})")
-        data = String(data).replace("//newSettersGetter" + "s", "//newSettersGetter" + "" + "s\n\napp.get('/" + req.body.schemaName + "', (req, res) => {\n\t" + req.body.schemaName + ".find({}).then((docs) => {\n\t\tconsole.log(docs);\n\t\tres.send(docs);\n\t})\n});")
-        data = String(data).replace("//newSettersGetter" + "s", "//newSettersGetter" + "" + "s\n\napp.get('/" + req.body.schemaName + "/:id', (req, res) => {\n\t" + req.body.schemaName + ".find({_id:ObjectId(req.params.id)}).then((docs) => {\n\t\tconsole.log(docs);\n\t\tres.send(docs);\n\t})\n});")
+        data = String(data).replace("//newSettersGetter" + "s", "//newSettersGetter" + "" + "s\n\napp.post('/" + req.body.schemaName + "', (req, res) => {\n\tvar obj1 = new " + req.body.schemaName + "(req.body);\n\obj1.save().then((doc) => {\n\t\tres.send(`${doc}`);\n\t})\n})")
+        data = String(data).replace("//newSettersGetter" + "s", "//newSettersGetter" + "" + "s\n\napp.get('/" + req.body.schemaName + "', (req, res) => {\n\t" + req.body.schemaName + ".find({}).then((docs) => {\n\t\tres.send(docs);\n\t})\n});")
+        data = String(data).replace("//newSettersGetter" + "s", "//newSettersGetter" + "" + "s\n\napp.get('/" + req.body.schemaName + "/:id', (req, res) => {\n\t" + req.body.schemaName + ".find({_id:ObjectId(req.params.id)}).then((docs) => {\n\t\tres.send(docs);\n\t})\n});")
         fs.writeFile('./app.js', data, (err) => {
-            console.log(err);
 
             res.send({
                 "status": "Initiated"
@@ -47,19 +70,18 @@ app.post('/objects', (req, res) => {
 
         })
     })
-    console.log("**/objects exited**");
 })
 
 app.put('/objects/:id', (req, res) => {
-    console.log("**/objects entered**");
+    //logger("API", "objects", "", req.body.schemaName, "success", "", req.connection.remoteAddress, "PUT");
 
     fs.appendFile('./schemas/' + req.body.schemaName + '.js', "var mongoose=require('mongoose');\nvar " + req.body.schemaName + '=mongoose.model("' + req.body.schemaName + '",{"' + req.body.schemaName + '":[' + JSON.stringify(req.body.schemaStructure) + '],"instanceId":{"type":"String"}});\nmodule.exports={' + req.body.schemaName + "}", (err) => {
     })
     fs.readFile('./app.js', (err, data) => {
         data = String(data).replace("//newImports", "//newImports\nvar {" + req.body.schemaName + "}=require('./schemas/" + req.body.schemaName + "')");
-        data = String(data).replace("//newSettersGetter" + "s", "//newSettersGetter" + "" + "s\n\napp.post('/" + req.body.schemaName + "', (req, res) => {\n\tconsole.log(req.body);\n\tvar obj1 = new " + req.body.schemaName + "(req.body);\n\tconsole.log(obj1)\n\obj1.save().then((doc) => {\n\t\tres.send(`${doc}`);\n\t})\n})")
-        data = String(data).replace("//newSettersGetter" + "s", "//newSettersGetter" + "" + "s\n\napp.get('/" + req.body.schemaName + "', (req, res) => {\n\t" + req.body.schemaName + ".find({}).then((docs) => {\n\t\tconsole.log(docs);\n\t\tres.send(docs);\n\t})\n});")
-        data = String(data).replace("//newSettersGetter" + "s", "//newSettersGetter" + "" + "s\n\napp.get('/" + req.body.schemaName + "/:id', (req, res) => {\n\t" + req.body.schemaName + ".find({_id:ObjectId(req.params.id)}).then((docs) => {\n\t\tconsole.log(docs);\n\t\tres.send(docs);\n\t})\n});")
+        data = String(data).replace("//newSettersGetter" + "s", "//newSettersGetter" + "" + "s\n\napp.post('/" + req.body.schemaName + "', (req, res) => {\n\tvar obj1 = new " + req.body.schemaName + "(req.body);\n\obj1.save().then((doc) => {\n\t\tres.send(`${doc}`);\n\t})\n})")
+        data = String(data).replace("//newSettersGetter" + "s", "//newSettersGetter" + "" + "s\n\napp.get('/" + req.body.schemaName + "', (req, res) => {\n\t" + req.body.schemaName + ".find({}).then((docs) => {\n\t\tres.send(docs);\n\t})\n});")
+        data = String(data).replace("//newSettersGetter" + "s", "//newSettersGetter" + "" + "s\n\napp.get('/" + req.body.schemaName + "/:id', (req, res) => {\n\t" + req.body.schemaName + ".find({_id:ObjectId(req.params.id)}).then((docs) => {\n\t\tres.send(docs);\n\t})\n});")
         fs.writeFile('./app.js', data, (err) => {
 
             res.send({
@@ -68,12 +90,10 @@ app.put('/objects/:id', (req, res) => {
         })
 
     })
-    console.log("**/objects exited**");
 })
 
 app.get('/objects/:id/:name', (req, res) => {
-
-    // getObjects(req.params.id, req.params.name, req.query.mode, req.query.filter || "", req.connection.remoteAddress, jsonwebtoken.verify(req.cookies.token, jwt_key).userId, "actual", res, "", "", "", "", req.cookies.token);
+    //logger("API", "objects", req.params.name, req.params.id, "success", "", req.connection.remoteAddress, "GET");
 
     filterText = req.query.filterText
     mode = req.query.mode;
@@ -81,21 +101,16 @@ app.get('/objects/:id/:name', (req, res) => {
     name = req.params.name
     callMode = req.query.mode
     id = req.params.id
-    console.log(search);
-    console.log(mode);
     if (filterText.length > 0 && mode != "undefined" && mode != undefined && mode == "showAll") {
-        console.log("RE");
         // eval(name + '.find(' + (search) + ').then((docs)=>{if(callMode=="actual"){res.send(docs)}else{return docs;}})');
         eval(name + '.find(' + search + ').then((docs)=>{res.send(docs)})');
     }
     else if (filterText.length == 0 && mode != "undefined" && mode != undefined && mode == "showAll") {
-        console.log("HERE");
         eval(name + '.find().then((docs)=>{res.send(docs)})');
     }
     else {
         // eval(name + '.findById("' + id + '",(err,docs)=>{if(callMode=="actual"){res.send(docs)}else{doACall(url,method,docs,instanceId,wid,name);return docs;}})');
-       console.log(name+"#"+id); 
-            eval(name + '.findById("' + id + '",(err,docs)=>{console.log(docs);res.send(docs)})');
+        eval(name + '.findById("' + id + '",(err,docs)=>{res.send(docs)})');
     }
 });
 
@@ -103,14 +118,13 @@ app.get('/objects/:id/:name', (req, res) => {
 var oldObjects = [];
 
 app.post('/addObject/:instanceId/:key', (req, res) => {
-    console.log("$$");
+    //logger("API", "addObject", req.params.key, req.params.instanceId, "success", "", req.connection.remoteAddress, "POST");
+
     key = req.params.key;
     instanceId = req.params.instanceId;
     var body = ('{"' + key + '":' + JSON.stringify(req.body) + ',"instanceId":"' + instanceId + '"}');
     var obj2 = eval("new " + key + '(JSON.parse(body))')
     obj2.save().then((doc) => {
-        console.log("#####");
-        console.log(doc);
         res.send(doc)
     })
 });
--- db/database.js
+++ db/database.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose')
 
 
 var url = process.env.OBJ_MONGODB_URL || "mongodb://localhost:27017/objs"
-//var url = "mongodb://localhost:27017"
-//var url="mongodb://localhost:27017"
+
 mongoose.connect(url, () => {
     console.log("DB connected successfully @ ", url);
 })
+
 
 module.exports = {
     mongoose
